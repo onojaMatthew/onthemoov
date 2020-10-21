589e0e5e215225bf4be5b9e3904458ed60754037
onojaMatthew <onojamatthew59@gmail.com>
2020-10-21 16:34:21 +0100

    added new updates

---
+++ client/build/precache-manifest.4297b13cd6500c24cd9446c40fba50c3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d1f9091924d77f7fbcd980859c3249",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8b6627a2301f3ee05d5",
+    "url": "/static/css/2.8bb14abc.chunk.css"
+  },
+  {
+    "revision": "7400cbc38912b8f786ed",
+    "url": "/static/css/main.ead8a79e.chunk.css"
+  },
+  {
+    "revision": "e8b6627a2301f3ee05d5",
+    "url": "/static/js/2.1827224c.chunk.js"
+  },
+  {
+    "revision": "7e3846720c92ec9ba89af7315cfdd2fd",
+    "url": "/static/js/2.1827224c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7400cbc38912b8f786ed",
+    "url": "/static/js/main.49928d07.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "4a763387896c7eb39aac8825fdac4c36",
+    "url": "/static/media/avatar.4a763387.png"
+  },
+  {
+    "revision": "59cb924741dda92eee9f1d3ce2921a51",
+    "url": "/static/media/banner.59cb9247.png"
+  },
+  {
+    "revision": "edc6587c5559dc6089967d8a8553dcbd",
+    "url": "/static/media/formbanner.edc6587c.png"
+  },
+  {
+    "revision": "68fd8acee52c4654be0f932a15e6ebb4",
+    "url": "/static/media/onthemoov_logo2.68fd8ace.png"
+  },
+  {
+    "revision": "8d922e8b4b343861bfa800e0847dae5b",
+    "url": "/static/media/user_avatar.8d922e8b.jpg"
+  }
+]);
